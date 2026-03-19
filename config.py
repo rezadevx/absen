56d7850876bb2c81e37d86f9ccfc5fd4f2d67831
rezadevs <sawe57192@gmail.com>
2026-03-20 01:30:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_ID = 23855532  # Ganti dengan API ID kamu dari https://my.telegram.org
 API_HASH = "3cc6eac0a9fbfe0b2b1da77f043cc9c9"  # Ganti dengan API HASH kamu
-BOT_TOKEN = "7900563287:AAHb1Gqtp-qRysNXvg1J7NvlCxQ5gmWyBtw"  # Ganti dengan Bot Token dari BotFather
+BOT_TOKEN = "8756289646:AAHMmiz7HmPzsTOCHuxOk3HTk5JbdZfCvRM"  # Ganti dengan Bot Token dari BotFather
