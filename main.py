b26def02700fb0745971afabe97ae1c39fec9eed
rezadevs <sawe57192@gmail.com>
2026-03-20 01:43:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from datetime import datetime
 import pytz
+import tzdata
 import config
 import os
 import sys
